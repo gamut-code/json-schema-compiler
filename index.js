266cc532921d7996a2c75aaeafba258445e72b4e
Kevin Altman <kevin@itsthatguy.com>
2017-09-05 15:29:34 -0400

    adds clean directive

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const DEFAULT_COMPILE_DIR = 'contracts/';
 
 args
 .option('output', 'The directory to compile the contracts into', DEFAULT_COMPILE_DIR)
-
-const flags = args.parse(process.argv)
+.option('clean', 'Removes the specified directory', DEFAULT_COMPILE_DIR)
 
 const glob = require('glob');
 const path = require('path');
 const fs = require('fs-extra');
+const { existsSync } = require('fs');
 
 const saveToFile = (data, filename, dir = 'data/') => {
   const projectRoot = path.resolve(process.cwd());
@@ -23,8 +23,24 @@ const saveToFile = (data, filename, dir = 'data/') => {
   return fs.writeFileSync(outputPath, JSON.stringify(data, null, 2));
 };
 
+const flags = args.parse(process.argv);
 const pathGlob = args.sub[0] || '**/*.contract.js';
-const outputDir = args.output || DEFAULT_COMPILE_DIR;
+const outputDir = flags.output;
+const cleanDir = flags.clean;
+
+if (flags.clean) {
+  try {
+    if (existsSync(cleanDir)) {
+      fs.removeSync(cleanDir);
+      console.log(`Removed directory: ${cleanDir}`);
+    } else {
+      console.warn(`Directory '${cleanDir}' does not exist, nothing to clean.`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 const options = {
   ignore: [
     'node_modules/**/*',
